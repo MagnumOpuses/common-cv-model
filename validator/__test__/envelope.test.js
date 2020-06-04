5a1e6e7f6637c3f7c0e6b00b3708c5adf989b6a7
Anton Wiklund <anton.wiklund@arbetsformedlingen.se>
2020-06-04 12:18:34 +0200

    Update envelope (#30)
    Updated envelope schema and example. Validation test now passes too.
    
    Co-authored-by: Markella Nikolopoulou-Themeli <markella.nikolopoulou-themeli@arbetsformedlingen.se>

--- validator/__test__/envelope.test.js
+++ validator/__test__/envelope.test.js
@@ -1,10 +1,11 @@
 const { validate } = require("../validator");
 
-test("example_WithHROpen421.json", (done) => {
+test("example_WithHROpen421JSON.json", (done) => {
   return validate(
-    "../envelope/example_WithHROpen421.json",
+    "../envelope/example_WithHROpen421JSON.json",
     "../envelope/DataEnvelope.json"
   ).then((result) => {
+    console.log(result.errors);
     expect(result.errors.length).toBe(0);
     done();
   });
